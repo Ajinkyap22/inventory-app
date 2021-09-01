b2bba7e0051ef21940663d4f6f0401308a73bfca
Ajinkyap22 <palaskarap15@gmail.com>
2021-09-01 19:19:10 +0530

    Final Commit before deploying

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
