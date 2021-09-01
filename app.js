def134b915651d6feed9de8330ead16c7d1674ab
Ajinkyap22 <palaskarap15@gmail.com>
2021-09-01 16:18:25 +0530

    Last Commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(helmet());
-app.use(compression()); //Compress all routes
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
